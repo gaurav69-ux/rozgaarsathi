6456b1c2e46a621ce14a55742431fab41909b60f
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-07-12 23:13:23 +0530

    DB configured

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectionDb = async () => {
   try {
     const mongoInstance = await mongoose.connect(
-      `${process.env.MONGO_CNN}/jobportal`
+      `${process.env.MONGO_URL}/jobportal`
     );
     console.log("MongoDb connected: " + mongoInstance.connection.host);
   } catch (err) {
